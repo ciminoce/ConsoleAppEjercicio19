64e84b5689c84dc6c9e8bdffb953068d57dc174e
ciminoce <ciminoce@yahoo.com.ar>
2022-06-07 10:19:29 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleAppEjercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ver si un número es par o impar";
+            Console.Write("Ingrese un número entero cualquiera:");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero%2==0)
+            {
+                Console.WriteLine($"{numero} es par");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} es impar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
